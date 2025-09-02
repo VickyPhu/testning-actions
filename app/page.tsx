a10372a1f9673c74a2c6db1f9bc4bf09a2bd7930
VickyPhu <victoriaphu@gmail.com>
2025-09-02 10:27:41 +0200

    add h1

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import { db } from "@/prisma/db";
 import TodoList from "./ui/todo-list";
 
 export default async function Home() {
-  const todos = await db.todo.findMany();
+	const todos = await db.todo.findMany();
 
-  return (
-    <main>
-      <TodoList defaultTodos={todos} />
-    </main>
-  );
+	return (
+		<main className="p-4">
+			<h1 className="text-5xl font-bold mb-4">Things todo</h1>
+			<TodoList defaultTodos={todos} />
+		</main>
+	);
 }
